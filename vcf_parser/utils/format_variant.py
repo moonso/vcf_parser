2c5e88a046380c27734a535d42517005fee651a7
Mans Magnusson <monsunas@gmail.com>
2015-04-21 21:26:01 +0200

    Added files in vcf_parser/utils

---
+++ vcf_parser/utils/format_variant.py
@@ -0,0 +1,95 @@
+from __future__ import unicode_literals
+from logging import getLogger
+
+from vcf_parser import Genotype
+from vcf_parser.utils import (build_info_dict, build_vep_annotation, 
+build_compounds_dict, build_rank_score_dict)
+
+def format_variant(line, individuals, vcf_header, vep_header):
+    """
+    Yield the variant in the right format. 
+    
+    If the variants should be splitted on alternative alles one variant 
+    for each alternative will be yielded.
+    
+    Arguments:
+        line (str): A string that represents a variant line in the vcf format
+    
+    Yields:
+        variant (dict): A dictionary with the variant information. The number
+                        of variants yielded depends on if split variant is used
+                        and how many alternatives there are
+    """
+    logger = getLogger(__name__)
+    
+    variant_line = line.rstrip().split('\t')
+    variant = dict(zip(vcf_header, line.rstrip().split('\t')))
+    
+    # A dictionary with the vep information
+    variant['vep_info'] = {}
+    # A dictionary with the genetic models (family ids as keys)
+    variant['genetic_models'] = {}
+    # A dictionary with genotype objects (individual ids as keys)
+    variant['genotypes'] = {}
+    # A dictionary with the compounds (family ids as keys)
+    variant['compound_variants'] = {}
+    # A dictionary with the rank scores (family ids as keys)
+    variant['rank_scores'] = {}
+    
+    variant['individual_scores'] = {}
+    
+    alternatives = variant['ALT'].split(',')
+    
+    variant['info_dict'] = build_info_dict(variant.get('INFO', '')) 
+    
+    #################### Some fields require special parsing ###########################
+    
+    ##### VEP ANNOTATIONS #####
+    if 'CSQ' in info_dict:
+        variant['vep_info'] = build_vep_annotation(
+                    info_dict['CSQ'], 
+                    variant['REF'], 
+                    alternatives
+                )
+    
+    ##### GENMOD ANNOTATIONS #####
+    
+    if 'GeneticModels' in info_dict:
+        variant['genetic_models'] = build_models_dict(
+                        info_dict['GeneticModels'])
+
+    if 'Compounds' in info_dict:
+        variant['compound_variants'] = build_compounds_dict(
+                         info_dict['Compounds'])
+
+    if 'RankScore' in info_dict:
+        variant['rank_scores'] = build_rank_score_dict(
+                            info_dict['RankScore'])
+    
+    if 'IndividualRankScore' in info_dict:
+        variant['individual_scores'] = build_rank_score_dict(
+                                    info_dict['IndividualRankScore'])
+    
+    ##### GENOTYPE ANNOTATIONS #####
+    
+    gt_format = variant.get('FORMAT', '').split(':')
+    
+    for individual in individuals:
+        gt_info = variant[individual].split(':')
+        gt_call = dict(zip(gt_format, gt_info))
+        
+        #Create a genotype object for this individual
+        genotype_dict[individual] = Genotype(**gt_call)
+    
+    variant['genotypes'] = genotype_dict
+    
+    variant['variant_id'] = '_'.join(
+                                [
+                                    variant['CHROM'],
+                                    variant['POS'],
+                                    variant['REF'],
+                                    alternatives[0]
+                                ]
+                            )
+    
+    return variant
---
+++ vcf_parser/utils/split_genotype.py
@@ -0,0 +1,79 @@
+from __future__ import unicode_literals
+from logging import getLogger
+
+def split_genotype(genotype, gt_format, alternative_number):
+    """
+    Take a genotype call and make a new one that is working for the new
+    splitted variant
+    
+    Arguments:
+        genotype (str): The original genotype call
+        gt_format (str): The format of the gt call
+        alternative_number (int): What genotype call should we return
+    
+    Returns:
+        new_genotype (str): A string that represents the new genotype
+    """
+    logger = getLogger(__name__)
+    splitted_genotype = genotype.split(':')
+    splitted_gt_format = gt_format.split(':')
+    new_genotype = []
+    phased = False
+    for number, genotype_info in enumerate(splitted_genotype):
+        gt_info = splitted_gt_format[number]
+        if gt_info == 'GT':
+            if '/' in genotype_info:
+                gt = genotype_info.split('/')
+            else:
+                gt = genotype_info.split('|')
+                phased = True
+            ref_allele = '.'
+            alt_allele = '.'
+            try:
+                # Check the ref Allele
+                if gt[0] != '.' and gt[1] != '.':
+                    ref_allele = '0'
+                    alt_allele = '0'
+                    if gt[0] == gt[1]:
+                        # In this case we have a homozygous call:
+                        if int(gt[0]) == alternative_number + 1:
+                            ref_allele = '1'
+                            alt_allele = '1'
+                    else:
+                        if (int(gt[0]) == alternative_number + 1 or 
+                            int(gt[1]) == alternative_number + 1):
+                            alt_allele = '1'                        
+                else:
+                # We now know that at least one of the alleles are uncalled
+                    if gt[0] != '.':
+                        if int(gt[0]) == alternative_number + 1:
+                            ref_allele = '1'
+                        else:
+                            ref_allele = '0'
+                    elif gt[1] != '.':
+                        if int(gt[1]) == alternative_number + 1:
+                            alt_allele = '1'
+                        else:
+                            alt_allele = '0'
+            except (ValueError, KeyError):
+                pass
+            
+            if phased:
+                new_genotype.append('|'.join([ref_allele,alt_allele]))
+            else:
+                new_genotype.append('/'.join([ref_allele,alt_allele]))            
+        elif gt_info == 'AD':
+            ad = []
+            # The reference depth will allways be the original depth now
+            ad.append(genotype_info.split(',')[0])
+            ad.append(genotype_info.split(',')[alternative_number+1])
+            new_genotype.append(','.join(ad))
+        elif gt_info == 'DP':
+            new_genotype.append(genotype_info)
+        elif gt_info == 'PL':
+            new_genotype.append(genotype_info)
+        else:
+            # There are several cases that we do not know how to handle yet so we just add the information
+            new_genotype.append(genotype_info)
+            
+    return ':'.join(new_genotype)
