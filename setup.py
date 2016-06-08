4a052fbfc103d056d488e974f49255a463a1de6b
moonso <monsunas@gmail.com>
2016-06-08 13:28:26 +0200

    Bumpes version to 1.5.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 long_description = 'Tool for parsing Variant Call Format (VCF) files. Works like a lightweight version of PyVCF.'
 
 setup(name='vcf_parser',
-    version='1.5.5',
+    version='1.5.6',
     description='Parsing vcf files',
     author = 'Mans Magnusson',
     author_email = 'mans.magnusson@scilifelab.se',
